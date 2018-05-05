edf7a99f93bc733348eac1c740dce7453e2ac9fa
Daniel del Castillo <dadelcas@hotmail.com>
2018-05-05 14:25:25 +0100

    [issue-4] Accept media types XML and JSON

--- prime-number-service-ws/src/main/java/com/company/prime/service/number/ws/controller/PrimeNumberController.java
+++ prime-number-service-ws/src/main/java/com/company/prime/service/number/ws/controller/PrimeNumberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,7 +28,9 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @Controller
-@RequestMapping(path = "/primes", produces = "application/json")
+@RequestMapping(path = "/primes", produces = {
+    MediaType.APPLICATION_JSON_VALUE,
+    MediaType.APPLICATION_XML_VALUE })
 @Api(description = "Prime number generator")
 @ResponseBody
 @Timed
